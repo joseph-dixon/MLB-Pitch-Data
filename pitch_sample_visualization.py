746166ee3da7906b0eae67b4ba6679650eb78718
Joseph Dixon <jpdixon95@gmail.com>
2020-10-21 13:43:05 -0600

    Added visualization files

--- pitch_sample_visualization.py
+++ pitch_sample_visualization.py
@@ -4,6 +4,9 @@ import plotly.express as px
 pitches = pd.read_csv("/Users/josephdixon/Desktop/Data Projects/Pitches/transformed_pitches.csv")
 pitches['Avg Horizontal Break'] = pitches['Avg Horizontal Break'].abs()
 
-fig = px.scatter_3d(pitches, x='Avg Speed', y='Avg Horizontal Break', z='Avg Vertical Break',
+#random sample of 100 pitches
+pitch_sample = pitches.sample(n = 100, replace = False, random_state = 1)
+
+fig = px.scatter_3d(pitch_sample, x='Avg Speed', y='Avg Horizontal Break', z='Avg Vertical Break',
               color='Pitch Type',size = 'Avg Spin')
 fig.show()
